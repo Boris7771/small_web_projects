268170abec370431f07515aa601beb64af3cb14d
Boris7771 <149595217+Boris7771@users.noreply.github.com>
2024-02-03 20:53:37 +0200

    recipe book food :green_salad:

---
+++ 0003_Recipe_book_APP/script.js
@@ -0,0 +1,47 @@
+const APY_KEY = "api_key=YOUR_API_KEY";
+
+function displayRecipes(recipes) {
+  const recipeListEl = document.getElementById("recipe-list");
+  recipeListEl.innerHTML = "";
+  recipes.forEach((recipe) => {
+    const recipeItemEl = document.createElement("li");
+    recipeItemEl.classList.add("recipe");
+
+    recipeImageEl = document.createElement("img");
+    recipeImageEl.src = recipe.image;
+    recipeImageEl.alt = recipe.title;
+    recipeItemEl.appendChild(recipeImageEl);
+
+    // recipeTitleEl = document.createElement("h2");
+    // recipeTitleEl.textContent = recipe.title;
+    // recipeItemEl.appendChild(recipeTitleEl);
+    recipeTitleEl = `<h2>${recipe.title}</h2>`;
+    recipeItemEl.innerHTML += recipeTitleEl;
+    recipeIngredientsEl = document.createElement("p");
+    recipeIngredientsEl.innerHTML += `<strong>Ingredients:</strong> ${recipe.extendedIngredients
+      .map((ingredient) => ingredient.original)
+      .join(", ")}`;
+    recipeItemEl.appendChild(recipeIngredientsEl);
+
+    recipeLinkEl = document.createElement("a");
+    recipeLinkEl.href = recipe.sourceUrl;
+    recipeLinkEl.textContent = "View Recipe";
+    recipeItemEl.appendChild(recipeLinkEl);
+    recipeListEl.appendChild(recipeItemEl);
+  });
+}
+async function getRecipes() {
+  const response = await fetch(
+    `https://api.spoonacular.com/recipes/random?number=5&apiKey=${APY_KEY}`
+  );
+
+  const data = await response.json();
+  return data.recipes;
+}
+
+async function init() {
+  const recipes = await getRecipes();
+  displayRecipes(recipes);
+}
+
+init();
